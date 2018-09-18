567856c94e7d843d6236a33c00f0248c0379178c
Spandana Velaga <spvelaga@gmail.com>
2018-09-18 11:50:02 -0400

    Created Dashboard component and added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+// Angular Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
 
+// Components Imports
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: '',redirectTo: '/dashboard',pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
